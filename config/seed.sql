9ec06f1d19015fece217a7ac13e747fc52d22d99
Linus Styrén <linus.styren@elev.ga.ntig.se>
2018-09-11 12:59:59 +0200

    added groups model

--- config/seed.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE public.users
-(
-    id serial PRIMARY KEY NOT NULL,
-    username varchar(255) NOT NULL,
-    encrypted_pw varchar(255) NOT NULL
-);
-CREATE UNIQUE INDEX users_id_uindex ON public.users (id);
-
