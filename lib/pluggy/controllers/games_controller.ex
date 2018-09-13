cd50d3d92ed26e1e0e007d649cbe65f3bb3510c0
Linus Styrén <linus.styren@elev.ga.ntig.se>
2018-09-13 12:17:37 +0200

    added score tracking

--- lib/pluggy/controllers/games_controller.ex
+++ lib/pluggy/controllers/games_controller.ex
@@ -9,8 +9,8 @@ defmodule Pluggy.GamesController do
     def learn(conn, id), do: game(conn, "games/learn", id)
     def name(conn, id), do: game(conn, "games/name", id)
     def face(conn, id), do: game(conn, "games/face", id)
-    def name(conn, id), do: game(conn, "games/hangman", id)
-    def face(conn, id), do: game(conn, "games/memory", id)
+    def hangman(conn, id), do: game(conn, "games/hangman", id)
+    def memory(conn, id), do: game(conn, "games/memory", id)
 
     def game(conn, path, id), do: send_resp(conn, 200, render(conn, path, people_json: Poison.encode!(Person.all(id))))
 
